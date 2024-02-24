3a0a4907821506e3c7d3cdc7e0150db226064d36
kotegran <158501231+kotegran@users.noreply.github.com>
2024-02-24 15:46:45 +0200

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,10 +1,21 @@
-import pygame
+from pygame import *
+
+init()
+
+win_width=500
+win_height=500
+FPS=60
+clock=time.Clock()
+exit=False
 
 # CREATING CANVAS
-window = pygame.display.set_mode((500, 700))
 
+bg = image.load("bg.png")
+bg = transform.scale(bg, (win_width, win_height))
+
+window = display.set_mode((win_width, win_height))
 # TITLE OF CANVAS
-pygame.display.set_caption("ping-pong")
+display.set_caption("ping-pong")
 exit = False
 
 class GameSprite(sprite.Sprite):
@@ -21,22 +32,56 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update_l(self,):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_W] and self.rect.y > 5:
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[pygame.K_S] and self.rect.y < 700:
+        if keys[K_S] and self.rect.y < 500:
             self.rect.y += self.speed
     def update_r(self,):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and self.rect.y > 5:
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[pygame.K_DOWN] and self.rect.y < 700:
+        if keys[K_DOWN] and self.rect.y < 500:
             self.rect.y += self.speed
 
-while not exit:
+class Ball(GameSprite):
+    def update(self, speed_x, speed_y):
+        self.rect.x += speed_x
+        self.rect.y += speed_y
 
-    canvas.blit(bg, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+player_r=Player("bg.png",0,100, 10, 80, 80)
+player_l=Player("bg.png",win_width - 80, 100, 10, 80, 80)
+ball = Ball("bg.png", 100, 100, 5, 50, 50)
+speed_x=5
+speed_y=5
+finish=False
+
+font = font.Font(None, 35)
+lose=1
+
+while exit != True:
+    for e in event.get():
+        if e.type == QUIT:
             exit = True
-    pygame.display.update()
+    window.blit(bg, (0, 0))
+    if finish != True:
+        if sprite.collide_rect(player_1,ball) or sprite.collide_rect(player_r,ball):
+            speed_x *= -1
+        if ball.rect.y > win_height - 80 or ball.rect.y < 0:
+            speed.y *= -1
+        if ball.rect.y < 0:
+            lose=font.render("Plater 1 lose", True, (180, 0, 0))
+            finish=True
+        if ball.rect.y > win_width:
+            lose=font.render("Plater 2 lose", True, (180, 0, 0))
+            finish=True
+        ball.update(speed_x,speed_y)
+        player_l.update_l()
+        plater_r.update_r()
+    else:
+        window.blit(lose, (250, 250))
+        player_l.reset()
+        plater_r.reset()
+        ball.reset()
+        display.update()
+        clock.tick(FPS)
